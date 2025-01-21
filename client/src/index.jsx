24962360528cc06b4e6f6375d3f425e10b16a2e8
Johnny Arroyo <jtarroyo2@gmail.com>
2025-01-20 22:34:26 -0700

    update Amplify configuration

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import './style.css'
 
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports'; // path to the aws-exports.js file
-
-// Configure AWS Amplify with the aws-exports.js settings
-Amplify.configure(awsconfig);
-
 const root = ReactDOM.createRoot(document.getElementById('root')) // Create a root
 
 root.render(
