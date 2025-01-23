9364b82a5da49fbb012e621701aba48b2f9e83f5
Johnny Arroyo <jtarroyo2@gmail.com>
2025-01-23 16:06:53 -0700

    updated fetchAPI to conditionally use baseUrl depending on prod or dev env

--- client/src/utils/fetchAPIData.js
+++ client/src/utils/fetchAPIData.js
@@ -1,15 +1,18 @@
 export const fetchAPIData = async (type) => {
-    try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/${type}`)
-        if (!response.ok) {
-            throw new Error(
-                `Error fetching data: ${response.statusText}`
-            )
-        }
-        const data = await response.json()
-        return await data
-    } catch (error) {
-        console.error('Error fetching data:', error)
-        return []
-    }
-}
+  try {
+      const baseURL = process.env.REACT_APP_API_URL || ''; // Use Amplify's REACT_APP_API_URL in production, fallback to relative path in development
+      const response = await fetch(`${baseURL}/api/${type}`);
+      
+      if (!response.ok) {
+          throw new Error(
+              `Error fetching data: ${response.statusText}`
+          );
+      }
+
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+  }
+};
